9063b668be5439a490b5522eb0601ee09f9e0bb2
Markus Gabriel <kontakt@markusgabriel.com>
2021-11-16 14:51:01 +0100

    hide dashboard widgets

--- wp-content/themes/wordpress-dev-theme/functions.php
+++ wp-content/themes/wordpress-dev-theme/functions.php
@@ -270,3 +270,23 @@ function remove_guttenberg_from_pages()
 {
     remove_post_type_support('page', 'editor');
 }
+
+
+// HIDE DASHBOARD WIDGETS
+add_action( 'wp_dashboard_setup', 'bt_remove_dashboard_widgets' );
+function bt_remove_dashboard_widgets() {
+
+    remove_meta_box( 'dashboard_primary','dashboard','side' ); // WordPress.com Blog
+    remove_meta_box( 'dashboard_plugins','dashboard','normal' ); // Plugins
+    remove_meta_box( 'dashboard_right_now','dashboard', 'normal' ); // Right Now
+    remove_action( 'welcome_panel','wp_welcome_panel' ); // Welcome Panel
+    remove_action( 'try_gutenberg_panel', 'wp_try_gutenberg_panel'); // Try Gutenberg
+    remove_meta_box('dashboard_quick_press','dashboard','side'); // Quick Press widget
+    remove_meta_box('dashboard_recent_drafts','dashboard','side'); // Recent Drafts
+    remove_meta_box('dashboard_secondary','dashboard','side'); // Other WordPress News
+    remove_meta_box('dashboard_incoming_links','dashboard','normal'); //Incoming Links
+    //remove_meta_box('rg_forms_dashboard','dashboard','normal'); // Gravity Forms
+    remove_meta_box('dashboard_recent_comments','dashboard','normal'); // Recent Comments
+    remove_meta_box('icl_dashboard_widget','dashboard','normal'); // Multi Language Plugin
+    //remove_meta_box('dashboard_activity','dashboard', 'normal'); // Activity
+}
