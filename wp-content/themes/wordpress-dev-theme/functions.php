7de89c72984cfe86b8f4d99e6cc4437380f974a9
Markus Gabriel <kontakt@markusgabriel.com>
2021-11-16 14:56:48 +0100

    hide menu items for editors

--- wp-content/themes/wordpress-dev-theme/functions.php
+++ wp-content/themes/wordpress-dev-theme/functions.php
@@ -290,3 +290,56 @@ function bt_remove_dashboard_widgets() {
     remove_meta_box('icl_dashboard_widget','dashboard','normal'); // Multi Language Plugin
     //remove_meta_box('dashboard_activity','dashboard', 'normal'); // Activity
 }
+
+
+// HIDE MENU ITEMS IN BACKEND FOR SPECIFIC USER ROLE
+function hide_menu() {
+    $user = wp_get_current_user();
+
+    // Check if the current user is an Editor
+    if ( in_array( 'wpseo_manager', (array) $user->roles ) ) {
+
+        // They're an editor, so grant the edit_theme_options capability if they don't have it
+        if ( ! current_user_can( 'edit_theme_options' ) ) {
+            $role_object = get_role( 'wpseo_manager' );
+            $role_object->add_cap( 'edit_theme_options' );
+        }
+
+        // Hide the Themes page
+        remove_submenu_page( 'themes.php', 'themes.php' );
+
+        // Hide the Widgets page
+        remove_submenu_page( 'themes.php', 'widgets.php' );
+
+        // Hide the Customize page
+        remove_submenu_page( 'themes.php', 'customize.php' );
+
+        // Remove Customize from the Appearance submenu
+        global $submenu;
+        unset($submenu['themes.php'][6]);
+    }
+
+    if ( in_array( 'editor', (array) $user->roles ) ) {
+
+        // They're an editor, so grant the edit_theme_options capability if they don't have it
+        if ( ! current_user_can( 'edit_theme_options' ) ) {
+            $role_object = get_role( 'editor' );
+            $role_object->add_cap( 'edit_theme_options' );
+        }
+
+        // Hide the Themes page
+        remove_submenu_page( 'themes.php', 'themes.php' );
+
+        // Hide the Widgets page
+        remove_submenu_page( 'themes.php', 'widgets.php' );
+
+        // Hide the Customize page
+        remove_submenu_page( 'themes.php', 'customize.php' );
+
+        // Remove Customize from the Appearance submenu
+        global $submenu;
+        unset($submenu['themes.php'][6]);
+    }
+}
+
+add_action('admin_menu', 'hide_menu', 10);
